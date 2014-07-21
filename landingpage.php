32bfd60eee3a3fd37a58a4b25bf53dd7821cf819
metallicmay <metallicmay@gmail.com>
2014-07-21 11:02:43 +0530

    Create landingpage.php

---
+++ landingpage.php
@@ -0,0 +1,12 @@
+<html>
+<head>
+<title> DELTA PROJECT </title>
+</head>
+<body style="background-color:#CCFF99;color:white;">
+<h1 align=center style="font-family:Verdana;"> WELCOME! </h1>
+<div align=center>
+<a href="registrationform.php"> <input type="button" style="background-color:green; color: white; font-family:Tahoma;" value="Sign up"></a>
+<br></br> <a href ="loginform.php"> <input type="button" style="background-color:green; color: white;font-family:Tahoma;" value="Log in"></a>
+</div>
+</body>
+</html>
