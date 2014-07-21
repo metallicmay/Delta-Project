19051dde029192a5694f18423be8dae9cc63c3ea
metallicmay <metallicmay@gmail.com>
2014-07-21 11:23:09 +0530

    Create procedures.php

---
+++ procedures.php
@@ -0,0 +1,19 @@
+<?php
+ include_once("database.php") 
+ ?>
+<?php
+session_start();
+if($_SESSION["name"]) {
+echo '<div align=right>' . $_SESSION["name"]. '</div>';	
+}
+echo"<a href=home.php>Home</a> <a href=about.php>About</a> <a href=procedures.php>Procedures</a> <a href=quicklinks.php>Quick Links</a> <a href=downloads.php>Downloads</a> <a href=faq.php>FAQ</a>";
+echo "<div align=right><a href=logout.php>Logout</a></div>";
+echo"<h1>PROCEDURES</h1>";	
+?>
+<html>
+<body style="background-color:#CCFF99;font-family:Verdana;">
+<p> General procedures: </p>
+<br></br>
+<p> Notices: </p>
+</body>
+</html>
