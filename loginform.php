f8be0066fa06f921c0cac82bccc5d25049b085c9
metallicmay <metallicmay@gmail.com>
2014-07-21 11:20:47 +0530

    Create loginform.php

---
+++ loginform.php
@@ -0,0 +1,18 @@
+<html>
+<body style="background-color:#CCFF99;font-family:Tahoma;"> 
+<h2 style="font-family:Verdana;"> Sign In </h2>
+<form action="loggedin.php" method="POST">
+   <table>
+   <tr>
+   <td> Roll number: </td> <td> <input type="text" name="rollnum"> </td>
+   </tr>
+   <tr>
+   <td> Password: </td> <td> <input type="password" name="pass"> </td>
+   </tr>
+   <tr>
+   <td> <input type="submit" value="Log in"> </td>
+   </tr>
+   </table>
+   </form>
+   </body>
+</html>
