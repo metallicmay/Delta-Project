d34fb8c7619429f9830a1cf5b170e4cd1582596f
metallicmay <metallicmay@gmail.com>
2014-07-22 15:02:33 +0530

    Update faq.php

--- faq.php
+++ faq.php
@@ -1,3 +1,9 @@
+<script>
+function toggle(id) {
+    var answer = document.getElementById(id);
+  (answer.style.display=='block') ? answer.style.display='none' : answer.style.display='block' ;  
+ }
+</script>
 <?php
  include_once("database.php") 
  ?>
@@ -5,19 +11,35 @@
 session_start();
 if($_SESSION["name"]) {
 echo '<div align=right>' . $_SESSION["name"]. '</div>';	
-}
 echo"<a href=home.php>Home</a> <a href=about.php>About</a> <a href=procedures.php>Procedures</a> <a href=quicklinks.php>Quick Links</a> <a href=downloads.php>Downloads</a> <a href=faq.php>FAQ</a>";
 echo "<div align=right><a href=logout.php>Logout</a></div>";
 echo"<h1>FAQ</h1>";	
+echo "<div class=ques1 onclick=toggle('ans1');> <a href=#>Question 1</a>
+    <div id=ans1> Answer 1</div>
+</div>
+<br></br>
+<div class=ques2 onclick=toggle('ans2');> <a href=#>Question 2</a>
+    <div id=ans2> Answer 2</div>
+</div>
+<br></br>
+<div class=ques3 onclick=toggle('ans3');> <a href=#>Question 3</a>
+    <div id=ans3> Answer 3</div>
+</div>
+<br></br>
+<div class=ques4 onclick=toggle('ans4');> <a href=#>Question 4</a>
+    <div id=ans4> Answer 4</div>
+</div>
+<br></br>
+<div class=ques5 onclick=toggle('ans5');> <a href=#>Question 5</a>
+    <div id=ans5> Answer 5</div>
+</div>";
+}
+else
+{ echo "You have been logged out! Click <a href=loginform.php> here </a> to log in again.";
+}
 ?>
 <html>
 <head>
-<script>
-function toggle(id) {
-    var answer = document.getElementById(id);
-  (answer.style.display=='block') ? answer.style.display='none' : answer.style.display='block' ;  
- }
-</script>
 <style>
 div.ques1
  { cursor:pointer; }
@@ -47,24 +69,5 @@ margin-left: 40px; }
 </style>
 </head>
 <body style="background-color:#CCFF99;font-family:Verdana;">
-<div class="ques1" onclick="toggle('ans1');"> <a href=#>Question 1</a>
-    <div id="ans1"> Answer 1</div>
-</div>
-<br></br>
-<div class="ques2" onclick="toggle('ans2');"> <a href=#>Question 2</a>
-    <div id="ans2"> Answer 2</div>
-</div>
-<br></br>
-<div class="ques3" onclick="toggle('ans3');"> <a href=#>Question 3</a>
-    <div id="ans3"> Answer 3</div>
-</div>
-<br></br>
-<div class="ques4" onclick="toggle('ans4');"> <a href=#>Question 4</a>
-    <div id="ans4"> Answer 4</div>
-</div>
-<br></br>
-<div class="ques5" onclick="toggle('ans5');"> <a href=#>Question 5</a>
-    <div id="ans5"> Answer 5</div>
-</div>
 </body>
 </html>
