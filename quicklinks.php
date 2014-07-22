16637a464bee955cdbb74e693b401c72024a8b0d
metallicmay <metallicmay@gmail.com>
2014-07-22 15:01:19 +0530

    Update quicklinks.php

--- quicklinks.php
+++ quicklinks.php
@@ -5,17 +5,20 @@
 session_start();
 if($_SESSION["name"]) {
 echo '<div align=right>' . $_SESSION["name"]. '</div>';	
-}
 echo"<a href=home.php>Home</a> <a href=about.php>About</a> <a href=procedures.php>Procedures</a> <a href=quicklinks.php>Quick Links</a> <a href=downloads.php>Downloads</a> <a href=faq.php>FAQ</a>";
 echo "<div align=right><a href=logout.php>Logout</a></div>";
 echo"<h1>QUICK LINKS</h1>";	
-?>
-<html>
-<body style="background-color:#CCFF99;font-family:Verdana;">
-<p> Important links: </p>
+echo "<p> Important links: </p>
 <div> <a href=http://www.nitt.edu/> Official Website of NITT </a> </div>
 <br></br>
 <p>Blogs about NITT:</p>
-<div> <a href=http://www.metallicmay.wordpress.com> NITT-The true blue experience</a> </div>
+<div> <a href=http://www.metallicmay.wordpress.com> NITT-The true blue experience</a> </div>";
+}
+else
+{ echo "You have been logged out! Click <a href=loginform.php> here </a> to log in again.";
+}
+?>
+<html>
+<body style="background-color:#CCFF99;font-family:Verdana;">
 </body>
 </html>
