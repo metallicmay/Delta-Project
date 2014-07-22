d77eeab95e21f38fbe599604fca45c60206fd56e
metallicmay <metallicmay@gmail.com>
2014-07-22 15:01:51 +0530

    Update downloads.php

--- downloads.php
+++ downloads.php
@@ -5,13 +5,16 @@
 session_start();
 if($_SESSION["name"]) {
 echo '<div align=right>' . $_SESSION["name"]. '</div>';	
-}
 echo"<a href=home.php>Home</a> <a href=about.php>About</a> <a href=procedures.php>Procedures</a> <a href=quicklinks.php>Quick Links</a> <a href=downloads.php>Downloads</a> <a href=faq.php>FAQ</a>";
 echo "<div align=right><a href=logout.php>Logout</a></div>";
 echo"<h1>DOWNLOADS</h1>";	
+echo "<p> Forms for download: </p>";
+}
+else
+{ echo "You have been logged out! Click <a href=loginform.php> here </a> to log in again.";
+}
 ?>
 <html>
 <body style="background-color:#CCFF99;font-family:Verdana;">
-<p> Forms for download: </p>
 </body>
 </html>
